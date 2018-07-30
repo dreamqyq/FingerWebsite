a3ab371992a9ed5e7aa9fd02138003d8aaf12a51
enoch <qyqdream@gmail.com>
2018-07-30 10:23:55 +0800

    finished index page V2.0

--- js/slideShow.js
+++ js/slideShow.js
@@ -1,68 +1,81 @@
-!function(){
-  let $view = $('.header_banner')
-  let controller = {
-    view : null,
-    slideBox : null,
-    size : null,
-    prevBtn : null,
-    nextBtn : null,
-    dotsBtn : null,
-    index : 0,
-    width : null,
-    init : function(view){
-      this.view = view
-      this.slideBox = this.view.children('.slideBox').eq(0)
-      this.dotsBtn = this.view.children('.dots').children()
-      this.prevBtn = this.view.children('.icon_prev')
-      this.nextBtn = this.view.children('.icon_next')
-      this.width = view.width()
-      this.size = this.dotsBtn.length
-      this.bindEvent()
-      this.btnClick()
-    },
-    bindEvent : function(){
-      this.startSlide()
-      this.view.on('mouseover',() => {
-        clearInterval(this.timer) 
-      })
-      this.view.on('mouseleave',() => {
-        this.startSlide()
-      })
-    },
-    startSlide : function(){
-      this.timer = setInterval(()=>{ 
-        this.index ++
-        this.changePic(this.index)
-      },2000)
-    },
-    changePic : function(index){
-      index = this.checkIndex(index)
-      this.slideBox.css('transform',`translateX(-${index*this.width}px)`)
-      this.dotsBtn
-        .eq(index).addClass('active')
-        .siblings().removeClass('active')  
-      this.index = index
-    },
-    btnClick : function(){
-      this.nextBtn.on('click',() => {
-        this.changePic(this.index + 1)
-      })
-      this.prevBtn.on('click',() => {
-        this.changePic(this.index - 1)
-      })
-      this.dotsBtn.on('click',(e) => {
-        let index = $(e.currentTarget).index()
-        this.changePic(index)
-      })
-    },
-    checkIndex : function(index){
-      if(index >= this.size){
-        index = 0
-      }else if(index < 0){
-        index = this.size - 1
-      }
-      return index
+slideShow($('.header_banner'))
+slideShow($('.main_banner > .banner'))
+function slideShow($view){
+  let n = 0
+  let $slideBox = $view.children('.slideBox').eq(0)
+  let $slidePic = $slideBox.children()
+  let $dotsBtn = $view.children('.dots').children('span')
+  let size = $slidePic.length
+  let timer = null
+  let width = $slidePic.width()
+  init()
+  startSlide()
+
+  $(document).on('visibilitychange',() => {
+    if(document.hidden){
+      window.clearInterval(timer)
+    }else{
+      startSlide()
     }
+  })
+
+  $($view).on('mouseover',function(){
+    clearInterval(timer)
+  })
+  $($view).on('mouseleave',function(){
+    startSlide()
+  })
+
+  $dotsBtn.on('click',function(e){
+    let index = $(e.currentTarget).index()
+    changePic(index)
+  })
+  $('.icon_next').on('click',function(){
+    changePic(n+1)
+  })
+  $('.icon_prev').on('click',function(){
+    changePic(n-1)
+  })
+
+  function init(){
+    let $picFirst = $slidePic.eq(0).clone(true)
+    let $picLast = $slidePic.eq(size-1).clone(true)
+    $slideBox.append($picFirst)
+    $slideBox.prepend($picLast)
+    $slideBox.css('transform',`translateX(-${width}px)`)
   }
-  controller.init($view)
-}.call()
+  function startSlide(){
+    timer = setInterval(() => {
+      changePic(n+1)
+    },2000)
+    return timer
+  }
+  function changePic(index){
+    if(index >= size){
+      index = 0
+    }else if(index < 0){
+      index = size-1
+    }
+    let previousIndex = n % size
+    if(previousIndex === 0 && index === size-1){
+      $slideBox.css('transform','translateX(0px)')
+               .one('transitionend',() => {
+                 $slideBox.hide().offset()
+                 $slideBox.css('transform',`translateX(-${size*width}px)`)
+                          .show()
+               })
+    }else if(previousIndex === size -1 && index === 0){
+      $slideBox.css('transform',`translateX(-${(size+1)*width}px)`)
+               .one('transitionend',() => {
+                 $slideBox.hide().offset()
+                 $slideBox.css('transform',`translateX(-${width}px)`)
+                          .show()
+               })
+    }else{
+      $slideBox.css('transform',`translateX(${-width*(index+1)}px)`)
+    } 
+    $dotsBtn.eq(index).addClass('active').siblings().removeClass('active')
+    n = index
+    console.log(n)
+  }
+}
