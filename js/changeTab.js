806cdbf7f416d93ed1bb63eb2c2dffbcc216ef0c
enoch <qyqdream@gmail.com>
2018-07-30 17:14:09 +0800

    optimized join-page js

--- js/changeTab.js
+++ js/changeTab.js
@@ -1,1 +1,15 @@
-alert(1)
+! function() {
+  let btn = document.querySelectorAll('.job-kind-menu-btn')
+  let details = document.querySelectorAll('.job-kind-details')
+  for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', function() {
+      for (let j = 0; j < btn.length; j++) {
+        btn[j].classList.remove('active')
+        details[j].classList.remove('active')
+      }
+      btn[i].classList.add('active')
+      details[i].classList.add('active')
+      details[i].parentNode.style.height = `${details[i].clientHeight}px`
+    })
+  }
+}.call()
